7a03853fbc323177a8a2bd73a183f9540383b2d8
accebenton <127049756+accebenton@users.noreply.github.com>
2025-04-10 19:36:55 +0100

    Add files via upload
    updated server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const sqlite3 = require('sqlite3').verbose();
 
 //express function to create web app
 const app = express();
+//tell express to use public folder cont html and css
+app.use(express.static('public'));
+
 //port variable to run browser locally, if port in use please change
 const port = 3000;
 
@@ -36,13 +39,21 @@ db.run(`
     )
   `);
 
+
+
+/*-------ADDING RISK TO DB---------------------*/
 //route for newly added risk to db--when user visits browser, it will add risk to table
 //SHOULD CHANGE THIS TO POST------------------------------
 app.get('/addrisk', (req, res) => {
-    // example data --- update later to take data from html
-    const title = 'outdated software, to update';
-    const likelihood = 3; // Scale of 1–5
-    const impact = 4;     // Scale of 1–5
+    // example data --- update later to take data from html, testing from url right now
+    const title = req.query.title;
+    const likelihood = req.query.likelihood; // scale of 1–5
+    const impact = req.query.impact;     // scale of 1–5
+
+    //check if all values are present
+    if (!title || !likelihood || !impact){
+      return res.send('Please provide title, likelihood and impact.')
+    }
 
     //calculate risk from OWASP matrix
     const riskScore = likelihood * impact;
@@ -56,7 +67,7 @@ app.get('/addrisk', (req, res) => {
         riskLevel = 'Medium';
     }
 
-    //insert in db
+    //insert into db
     db.run(
         `INSERT INTO risks (title, likelihood, impact, risk_level) VALUES (?, ?, ?, ?)`,
         [title, likelihood, impact, riskLevel],
@@ -70,3 +81,40 @@ app.get('/addrisk', (req, res) => {
           }
       );
     });
+
+/*-----------------VIEWING RISKS IN DB----------------------*/
+//when someone clicks on view risks, takes them to a new page which shows risks in table
+//request, coming from browser to server - response - send back to browser
+app.get('/viewrisks', (req, res) => {
+  const sql = 'SELECT * FROM risks';
+  db.all(sql, [], function(err, rows) {
+    //could change to return statement to stop before else clause runs
+    if (err) {
+      return res.send('Error loading risks.');
+    } 
+
+    let output = "Risks: \n";
+    
+    //loop through rows
+    for(let i = 0; i <rows.length; i++){
+      const risk = rows[i];
+      output += risk.title + '\n';
+    }
+
+    //show in browser as text just now, HTML later when updating code
+    res.type('text');
+    //sends output to browser
+    res.send(output);
+  });
+});
+
+
+//when user clicks on html that is linked with /home this code will run
+app.get('/home', (req, res) => {
+  // get all risks from db
+  const sql = `SELECT * FROM risks`;
+
+db.all(sql, [], function(err, rows) {
+    
+
+})
