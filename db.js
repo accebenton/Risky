58a38612641d37550843ccda36736a16c6c4e45a
accebenton <127049756+accebenton@users.noreply.github.com>
2025-08-02 17:03:00 +0100

    final touches before submitting to uni

--- db.js
+++ db.js
@@ -2,5 +2,5 @@
 //opens a single connection to db file and exports connection so other files can access
 //verbose is for extra debugging info
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('riskTool.db');
+const db = new sqlite3.Database('./db/riskTool.db');
 module.exports = db;
